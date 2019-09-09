13e83e8b5d42401c6605ad5e0eaff5aa00fab4ca
Med Red <midou95liverpool@gmail.com>
2019-09-09 20:50:29 +0100

    changed added

---
+++ resources/views/session/form.blade.php
@@ -0,0 +1,26 @@
+<div>
+	<div class="col-md-6 mb-3">
+	Formation:
+	<select class="form-control" name="formation_id">
+		<option value="">unsigned</option>
+	</select>
+	</div>	
+	<br/>
+	<div class="col-md-6 mb-3">
+	Professor:
+	<select class="form-control" name="professor_id">
+		<option value="">unsigned</option>
+	</select>
+	</div>
+	<br/>
+	 <div class="col-md-6 mb-3">
+      <label for=" ">Date start </label>
+  	  <input type="date" class="form-control"  name="start_date" value="">
+    </div>
+    <br>
+    <div class="col-md-6 mb-3">
+        <label for=" ">date end </label>
+        <input type="date" class="form-control" name="end_date" value="">
+    </div>
+    
+</div>
--- resources/views/session/index.blade.php
+++ resources/views/session/index.blade.php
@@ -41,12 +41,12 @@
             <td></td>
             <td>
               <div class="row">
-          <a href="{{route('session.edit', $session->id)}}"class="btn btn-info btn-bordred wave-light"> <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+          <a href="{{route('sessions.edit', $sessions->id)}}"class="btn btn-info btn-bordred wave-light"> <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
           </a>
 
           <a href="#" class="btn btn-warning btn-bordred wave-light" data-toggle="modal" data-target="#show"><i class="fa fa-eye" aria-hidden="true"></i></a>
 
-           <form method="POST" action="{{ route('session.destroy', $session->id) }}">
+           <form method="POST" action="{{ route('sessions.destroy', $sessions->id) }}">
             @csrf 
            {{ method_field('DELETE') }}
            <button type="submit" id="sa-remove" onclick="return confirm('Are you sure?')" class="wave-effect btn btn-danger btn-bordred wave-light"><i class="fa fa-times"></i></button>
@@ -59,6 +59,38 @@
 
     </div>
   </div>
-
 </div>
+
+
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+  Add New 
+</button>
+
+<!-- Add -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">New Session</h4>
+      </div>
+        <form action="{{ route('session.store')}}" method="post" enctype="multipart/form-data">
+          @csrf
+          <div class="modal-body">
+          @include('session.form')
+          </div>  
+           <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Save</button>
+      </div>
+
+        </form>
+      
+     
+    </div>
+  </div>
+</div>
+
 @endsection
