d7b167e2059814b93d13ab03ff96de9245282c2f
BERLIN <samiskoubi@gmail.com>
2019-10-01 14:33:33 +0100

    Front-end Changes :(Presence/All)

--- resources/views/presence/index.blade.php
+++ resources/views/presence/index.blade.php
@@ -1,87 +1,97 @@
 @extends('layouts.master')
 @section('content')
+    <div class="container-fluid mt--7">
+        <!-- Table -->
+        <div class="row">
+            <div class="col">
+                <div class="card shadow">
+                    <div class="card-header border-0">
+                        <h3 class="pt-2 mb-0 float-left">All Presences</h3>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary float-left ml-3" data-toggle="modal" data-target="#exampleModal">+
+                        </button>
+                        <div class="float-right">
+                            <form action="/search" method="GET" class="float-right pl-3">
 
+                                <div class="input-group input-group-alternative">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" style="background-color: #5e72e4;border-color: #5e72e4;"><button type="submit" class="fas fa-search" style="color: rgba(255, 255, 255, 1);background: #fff0;border-color: #fff0;cursor: pointer;"></button></span>
+                                    </div>
+                                    <input class="form-control p-2" type="search" placeholder="Search" name="search" style="border: 1px solid #e8e8e8;">
+                                </div>
+                            </form>
 
-<div class="">
-  <div class="box">
-    <div class="box-header">
-      <h3 class="box-title">All Presence</h3>
-      <div class="col-md-4">
-        <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Add New
-        </button>
-      </div>
-    </div>
-    <br/>
-    <div class="col-md-4 search">
-      <form action="/search" method="GET">
-        <div class="input-group">
-          <input type="search" name="search" class="form-control">
-          <span class="input-group-prepend">
-            <button type="submit" class="btn btn-primary bouton">Search</button>
-          </span>
-        </div>
-      </form>
-    </div>
-    <br/>
-    <div class="box-body">
-      
-            <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Class</th>
-            <th scope="col">Student</th>
-            <th scope="col">Presence</th>
-            <th scope="col">Action </th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach($presences as $presence)
-          <tr>
-            <th scope="row">{{$presence->id}}</th>
-            <td>{{$presence->seance->name}}</td>
-            <td>{{$presence->Student->lastname}}</td>
-            <td>{{$presence->presence}}</td>
-            <td>
-              <div class="row">
-          <a href="{{route('presence.show', $presence->id)}}"  class="btn btn-warning btn-bordred wave-light"><i class="fa fa-eye" aria-hidden="true"></i></a>
+                        </div>
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Add New Presence</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <form action="{{ route('presence.store')}}" method="post" enctype="multipart/form-data">
+                                        {{csrf_field()}}
+                                        <div class="modal-body">
+                                            <div class="form-group">
+                                                @include('presence.form')
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            <button type="submit" class="btn btn-primary">Save changes</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table align-items-center table-flush">
+                            <thead class="thead-light">
+                            <tr>
+                                <th scope="col">Id</th>
+                                <th scope="col">Class</th>
+                                <th scope="col">Student</th>
+                                <th scope="col">Presence</th>
+                                <th scope="col">Action </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($presences as $presence)
+                                <tr>
+                                    <th scope="row">
 
-           <form method="POST" action="{{ route('presence.destroy', $presence->id) }}" style="float: left !important;display: contents;">
-            @csrf 
-           {{ method_field('DELETE') }}
-           <button type="submit" id="sa-remove" onclick="return confirm('Are you sure?')" class="wave-effect btn btn-danger btn-bordred wave-light"><i class="fa fa-times"></i></button>
-          </form>
+                                        <div class="media-body">
+                                            <span class="mb-0 text-sm">{{$presence->id}}</span>
+                                        </div>
+
+                                    </th>
+                                    <td>{{$presence->seance->name}}</td>
+                                    <td>{{$presence->Student->lastname}}</td>
+                                    <td>{{$presence->presence}}</td>
+
+                                    <td class="align-items-center">
+                                        <a href="{{route('presence.show', $presence->id)}}"class="btn btn-light btn-sm btn-bordred wave-light"> <i class="fa fa-eye"></i>
+                                        </a>
+
+                                        <form method="POST" action="{{ route('presence.destroy', $presence->id) }}" style="float: left !important;display: contents;">
+                                            @csrf
+                                            {{ method_field('DELETE') }}
+                                            <button type="submit" id="sa-remove" onclick="return confirm('Are you sure?')" class="wave-effect btn btn-sm btn-light btn-bordred wave-light"><i class="fa fa-times"></i></button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="card-footer py-4">
+                        <div class="links">{{$presences->links()}}</div>
+                    </div>
+                </div>
             </div>
-          </td>
-          </tr>
-          @endforeach  
-        </table>
-
-    </div>
-  </div>
-</div>
-
-<div class="links">{{$presences->links()}}</div>
-<!-- Add -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Precense </h4>
-      </div>
-        <form action="{{ route('presence.store')}}" method="post" enctype="multipart/form-data">
-          @csrf
-          <div class="modal-body">
-          @include('presence.form')
-          <div class="modal-footer">
-         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-         <button type="submit" class="btn btn-primary">Save</button>
         </div>
-          </div>  
-        </form>
-    </div>
-  </div>
-</div>
 @endsection
